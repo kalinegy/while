c92a5e1b71445d8219209e59cc670719d1410d11
Kaline Gabrielly <gkaline769@gmail.com>
2025-05-13 11:01:17 -0300

    soma(erro)

---
+++ soma.py
@@ -0,0 +1,12 @@
+soma=0
+num=int(input("Digite números positivos(0 para parar):"))
+
+while num != 0:
+    if num > 0:
+        soma+= num
+    else:
+        print("Número negativo, tente novamente")
+    num= int(input("Digite outro número(0 para parar):"))
+    print(f"A soma é {soma}")
+
+#não soma
